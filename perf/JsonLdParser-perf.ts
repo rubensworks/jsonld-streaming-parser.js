9c45590ebdce30e2a55592bd408823295b60a2b7
Ruben Taelman <ruben.taelman@ugent.be>
2018-07-18 15:02:20 +0200

    Add basic performance tests

---
+++ perf/JsonLdParser-perf.ts
@@ -0,0 +1,19 @@
+import {readFileSync} from "fs";
+import * as path from "path";
+// tslint:disable-next-line:no-var-requires
+const jsonld = require('jsonld');
+
+const fileName = path.resolve(process.cwd(), process.argv[2]);
+const TIMER = 'Parsing file ' + fileName;
+
+// tslint:disable:no-console
+console.time(TIMER);
+
+jsonld.promises.toRDF(JSON.parse(readFileSync(fileName, 'utf-8')))
+  .then((plainQuads: any[]) => {
+    console.timeEnd(TIMER);
+    console.log('* Quads parsed: ' + plainQuads.length);
+    console.log('* Memory usage: ' + Math.round(process.memoryUsage().rss / 1024 / 1024) + 'MB');
+  }).catch((e: Error) => {
+  throw e;
+});
---
+++ perf/StreamingJsonLdParser-perf.ts
@@ -0,0 +1,23 @@
+import {JsonLdParser} from "../index";
+import {createReadStream} from "fs";
+import * as path from "path";
+
+const fileName = path.resolve(process.cwd(), process.argv[2]);
+const TIMER = 'Parsing file ' + fileName;
+
+// tslint:disable:no-console
+console.time(TIMER);
+
+let count = 0;
+const parsed = createReadStream(fileName).pipe(new JsonLdParser());
+parsed.on('data', (quad) => {
+  count++;
+});
+parsed.on('error', (e) => {
+  throw e;
+});
+parsed.on('end', () => {
+  console.timeEnd(TIMER);
+  console.log('* Quads parsed: ' + count);
+  console.log('* Memory usage: ' + Math.round(process.memoryUsage().rss / 1024 / 1024) + 'MB');
+});
