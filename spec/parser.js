db49e7f6ef146f2bf8de9cc8645e086648ab2c56
Ruben Taelman <ruben.taelman@ugent.be>
2019-02-07 16:27:19 +0100

    Run toRdf and error JSON-LD test manifests

---
+++ spec/parser.js
@@ -0,0 +1,17 @@
+const { JsonLdParser } = require("..");
+const { ErrorSkipped } = require('rdf-test-suite');
+
+module.exports = {
+  parse: function (data, baseIRI, options) {
+    if (options.processingMode && options.processingMode !== '1.0') {
+      return Promise.reject(
+        new ErrorSkipped(`Test with processing mode ${options.processingMode} was skipped, only 1.0 is supported.`));
+    }
+    if (options.specVersion && options.specVersion !== '1.0') {
+      return Promise.reject(
+        new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.0 is supported.`));
+    }
+    return require('arrayify-stream')(require('streamify-string')(data)
+      .pipe(new JsonLdParser(Object.assign({ baseIRI, allowOutOfOrderContext: true, validateValueIndexes: true }, options))));
+  },
+};
