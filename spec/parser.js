4717f20789b0ac90bb40aa38e133f530d93b2f5d
Ruben Taelman <ruben.taelman@ugent.be>
2019-03-26 15:02:28 +0100

    Configure spec testing for 1.1

--- spec/parser.js
+++ spec/parser.js
@@ -3,11 +3,11 @@ const { ErrorSkipped } = require('rdf-test-suite');
 
 module.exports = {
   parse: function (data, baseIRI, options) {
-    if (options.processingMode && options.processingMode !== '1.0') {
+    if (options.processingMode && (options.processingMode !== '1.0' && options.processingMode !== '1.1')) {
       return Promise.reject(
         new ErrorSkipped(`Test with processing mode ${options.processingMode} was skipped, only 1.0 is supported.`));
     }
-    if (options.specVersion && options.specVersion !== '1.0') {
+    if (options.specVersion && (options.specVersion !== '1.0' && options.specVersion !== '1.1')) {
       return Promise.reject(
         new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.0 is supported.`));
     }
