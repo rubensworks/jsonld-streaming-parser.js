ca51fff89382c79401f4f548cd89c7dd93b23deb
Ruben Taelman <rubensworks@gmail.com>
2019-10-05 19:52:48 +0200

    Restore coverage of EntryHandlerPredicate

--- test/entryhandler/EntryHandlerPredicate-test.ts
+++ test/entryhandler/EntryHandlerPredicate-test.ts
@@ -1,11 +1,45 @@
 import {EntryHandlerPredicate} from "../../lib/entryhandler/EntryHandlerPredicate";
+import {ParsingContext} from "../../lib/ParsingContext";
+import {Util} from "../../lib/Util";
 
 describe('EntryHandlerPredicate', () => {
   const handler = new EntryHandlerPredicate();
 
+  let parsingContext: ParsingContext;
+  let util: Util;
+
+  beforeEach(() => {
+    parsingContext = new ParsingContext(<any> {});
+    util = new Util({ parsingContext });
+  });
+
   describe('isPropertyHandler', () => {
     it('should return true', async () => {
       expect(handler.isPropertyHandler()).toBe(true);
+    });
+  });
+
+  describe('validate', () => {
+    it('should return false for empty keys', async () => {
+      expect(await handler.validate(parsingContext, util, [''], 0, false)).toBeFalsy();
+    });
+
+    it('should return false on blank node properties', async () => {
+      expect(await handler.validate(parsingContext, util, ['_:b'], 0, false)).toBeFalsy();
+    });
+
+    it('should return true on regular properties', async () => {
+      expect(await handler.validate(parsingContext, util, ['http://prop.com'], 0, false)).toBeTruthy();
+    });
+
+    it('should return true on compacted properties', async () => {
+      parsingContext.contextTree.setContext([], Promise.resolve({ prop: 'http://example.org/' }));
+      expect(await handler.validate(parsingContext, util, ['prop'], 0, false)).toBeTruthy();
+    });
+
+    it('should return false on compacted properties without known expansion', async () => {
+      parsingContext.contextTree.setContext([], Promise.resolve({ prop: 'http://example.org/' }));
+      expect(await handler.validate(parsingContext, util, ['propDifferent'], 0, false)).toBeFalsy();
     });
   });
 });
