846b8d31ffd0f297ec73d2a620649a2edb8f2eaf
Ruben Taelman <ruben.taelman@ugent.be>
2019-12-08 16:03:37 +0100

    Only run spec 1.1 tests

--- spec/parser.js
+++ spec/parser.js
@@ -7,9 +7,9 @@ module.exports = {
       return Promise.reject(
         new ErrorSkipped(`Test with processing mode ${options.processingMode} was skipped, only 1.0 is supported.`));
     }
-    if (options.specVersion && (options.specVersion !== '1.0' && options.specVersion !== '1.1')) {
+    if (options.specVersion && options.specVersion !== '1.1') {
       return Promise.reject(
-        new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.0 is supported.`));
+        new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.1 is supported.`));
     }
     return require('arrayify-stream')(require('streamify-string')(data)
       .pipe(new JsonLdParser(Object.assign({
