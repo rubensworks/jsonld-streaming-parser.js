4d4ca956a4695a0f36e363d4e11011d014c04aa2
Ruben Taelman <ruben.taelman@ugent.be>
2020-09-02 09:54:47 +0200

    Fix import method sometimes failing on large streams

--- lib/JsonLdParser.ts
+++ lib/JsonLdParser.ts
@@ -153,8 +153,8 @@ export class JsonLdParser extends Transform implements RDF.Sink<EventEmitter, RD
   public import(stream: EventEmitter): RDF.Stream {
     const output = new PassThrough({ readableObjectMode: true });
     stream.on('error', (error) => parsed.emit('error', error));
-    stream.on('data', (data) => output.write(data));
-    stream.on('end', () => output.emit('end'));
+    stream.on('data', (data) => output.push(data));
+    stream.on('end', () => output.push(null));
     const parsed = output.pipe(new JsonLdParser(this.options));
     return parsed;
   }
